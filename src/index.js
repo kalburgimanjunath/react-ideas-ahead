2057d81122015b088d32c97eb75f5040766c8dce
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2022-09-10 12:52:57 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ import { EarthoOneProvider } from '@eartho/one-client-react';
 // import { transitions, positions, Provider as AlertProvider } from 'react-alert';
 // import AlertTemplate from 'react-alert-template-basic';
 
-const options = {
-  timeout: 5000,
-  position: positions.BOTTOM_CENTER,
-};
+// const options = {
+//   timeout: 5000,
+//   position: positions.BOTTOM_CENTER,
+// };
 root.render(
   <StrictMode>
     <EarthoOneProvider clientId="TM0RDLKzk7fncWCdFnU1">
